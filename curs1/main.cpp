483699f321d18b05ef8c9efcb470c6d9e2578429
mihai <mihai.tuhari@gmail.com>
2024-11-04 21:16:31 +0200

    feat: laborator 1

--- curs1/main.cpp
+++ curs1/main.cpp
@@ -1,36 +1,13 @@
 #include <gl/freeglut.h>
 
-void init(void)
-{
+void init() {
     glClearColor(0.282f, 0.545f, 0.639f, 1.0f);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0.0, 400.0, 0.0, 300.0);
 }
 
-void desen(void) // procedura desenare
-{
-    glColor3f(0.0, 0.0, 1.0); // culoarea punctelor: albastru
-    glBegin(GL_POINTS); // reprezinta puncte
-    glVertex2i(20, 20);
-    glVertex2i(21, 21);
-    glVertex2i(22, 22);
-    glVertex2i(23, 23);
-    glVertex2i(24, 24);
-    glVertex2i(100, 100);
-    glEnd();
-
-    glColor3f(1.0, 0.0, 0.0); // culoarea liniei frante: rosu
-    glBegin(GL_LINE_STRIP);
-    glVertex2i(0, 0);
-    glVertex2i(200, 100);
-    glVertex2i(300, 120);
-    glEnd();
-
-    glFlush(); // proceseaza procedurile OpenGL cat mai rapid
-}
-
-
-void drawTextMIHAI() {
+// 2. Draw the text "MIHAI"
+void drawText() {
     glColor3f(1.0, 1.0, 1.0);
     glLineWidth(6.0);
 
@@ -38,7 +15,7 @@ void drawTextMIHAI() {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Litera M
+    // Draw letter M
     glBegin(GL_LINES);
     glVertex2i(10, 10);
     glVertex2i(10, 50);
@@ -50,13 +27,13 @@ void drawTextMIHAI() {
     glVertex2i(30, 10);
     glEnd();
 
-    // Litera I
+    // Draw letter I
     glBegin(GL_LINES);
     glVertex2i(40, 10);
     glVertex2i(40, 50);
     glEnd();
 
-    // Litera H
+    // Draw letter H
     glBegin(GL_LINES);
     glVertex2i(50, 10);
     glVertex2i(50, 50);
@@ -66,7 +43,7 @@ void drawTextMIHAI() {
     glVertex2i(60, 50);
     glEnd();
 
-    // Litera A
+    // Draw letter A
     glBegin(GL_LINES);
     glVertex2i(70, 10);
     glVertex2i(80, 50);
@@ -76,7 +53,7 @@ void drawTextMIHAI() {
     glVertex2i(85, 30);
     glEnd();
 
-    // Litera I
+    // Draw letter I
     glBegin(GL_LINES);
     glVertex2i(100, 10);
     glVertex2i(100, 50);
@@ -84,21 +61,84 @@ void drawTextMIHAI() {
 
     glDisable(GL_LINE_SMOOTH);
     glDisable(GL_BLEND);
+    glFlush();
+}
 
+// 3. Draw a cube with RGB colors
+void drawCubeRGB() {
+    glBegin(GL_QUADS);
+
+    // FRONT FACE
+    // Top-left corner: cyan
+    glColor3f(0.0, 1.0, 1.0);
+    glVertex2i(50, 200);
+
+    // Top-right corner: white
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(150, 200);
+
+    // Bottom-right corner: yellow
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex2i(150, 100);
+
+    // Bottom-left corner: green
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2i(50, 100);
+
+    // TOP FACE
+    // Bottom-left corner: cyan
+    glColor3f(0.0, 1.0, 1.0);
+    glVertex2i(50, 200);
+
+    // Top-left corner: cyan
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2i(100, 230);
+
+    // Top-right corner: magenta
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex2i(200, 230);
+
+    // Bottom-right corner: white
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(150, 200);
+
+    // RIGHT FACE
+    // Bottom-left corner: yellow
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex2i(150, 100);
+
+    // Bottom-right corner: red
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2i(200, 130);
+
+    // Top-right corner: magenta
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex2i(200, 230);
+
+    // Top-left corner: white
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2i(150, 200);
+
+    glEnd();
+    glFlush();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawText();
+    drawCubeRGB();
     glFlush();
 }
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(300, 300); // 1. Pozitionare schimbata
-    glutInitWindowSize(900, 700); // 2. Dimensiuni schimbate
-    glutCreateWindow("Puncte & Segmente - Mihai Tuhari");
+    glutInitWindowPosition(300, 300); // 1. Change window position
+    glutInitWindowSize(900, 700); // 1. Change window size
+    glutCreateWindow("Curs 1 - text/cub - Mihai Tuhari");
 
     init();
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutDisplayFunc(drawTextMIHAI);
+    glutDisplayFunc(display);
     glutMainLoop();
 
     return 0;
